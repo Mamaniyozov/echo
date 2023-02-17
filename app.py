62c550b079aea629e03da8ba8e8a21b9d213393f
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-02-16 20:48:58 -0800

    read

--- app.py
+++ app.py
@@ -1,42 +1,39 @@
-from flask import Flask ,request
+from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Filters
+from flask import Flask, request
 import requests
-
-from telegram import Bot, Update
-from telegram.ext import Dispatcher,CommandHandler,MessageHandler,Filters
 import os
+from telegram import Bot, Update
 
-from bot_app import(
+from main import (
     start,
-    echo,
+    echo
 )
-    
+
+# flask app
 app = Flask(__name__)
+
+# bot
 TOKEN = os.environ['TOKEN']
 bot = Bot(TOKEN)
 
-@app.route('/webhook',methods=['POST','GET'])
-
+@app.route('/webhook', methods=['POST', 'GET'])
 def webhook():
     if request.method == 'GET':
-        return 'Helo World'
+        return 'hi from Python-2022I'
+    # get data from request
     elif request.method == 'POST':
-
         data = request.get_json(force=True)
 
-        dispatcher:Dispatcher = Dispatcher(bot, None , workers=0)
-
-        update:Update = Update.de_json(data, bot)
-
-        dispatcher.add_handler(CommandHandler('start',callback=start))
-        dispatcher.add_handler(MessageHandler(Filters.text,echo))
+        # update
+        dispatcher: Dispatcher = Dispatcher(bot, None, workers=0)
+        update: Update = Update.de_json(data, bot)
+        
+        dispatcher.add_handler(CommandHandler('start', callback=start))
+        dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
         dispatcher.process_update(update)
+        return 'ok'
 
-        # chat_id = update.message.chat_id
-        # text = update.message.text
-        # if text !=None:
-
-        #     bot.send_message(chat_id, text)
-        # print(chat_id)
-
-    return "Assalomu alaykum"
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
